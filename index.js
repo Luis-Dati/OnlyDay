b39da04b5926b4f63cb4801de1e6d1b3e4b97b0c
Luis Dati <datduyle123456789@gmail.com>
2024-06-29 17:35:20 +0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 $('document').ready(function(){
-	
+	var url = "https://onlydayserver.onrender.com/respone";
+
 	$('#dayBtn').on('click',function(){
 		var day = $('#dayInput').val()
 		console.log(day)
-		if(day != "2007-06-15"){
+		if(day != "2007-07-01"){
 			$('#alert1').fadeIn('fast').fadeOut(4000)
 		} else {
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: {
+                    "content": "Program is starting!!!",
+                    "ps": "None"
+                }
+            }).done(function(ketqua) {
+                console.log(ketqua);
+            });
+
 			$('#loading').fadeOut('fast')
 			$('#main').fadeIn('fast')
 			$('body').addClass('peach')
@@ -18,7 +30,7 @@ $('document').ready(function(){
 	let isClick = false;
 	$('#play').click(function(){
 		if(!isClick){
-			const time = 1000;
+			const time = 3000;
 			$('.song')[0].play()
 			$('#play').delay(1000).fadeOut('slow').delay(2000).promise().done(()=>{
 				$('#message1').show().promise().done(()=>{
@@ -65,13 +77,92 @@ $('document').ready(function(){
 		$('#p8').fadeOut('slow')
 		$('#continue').fadeOut('slow').delay(1000).promise().done(()=>{
 			$('#heart').show(1).delay(2000).promise().done(()=>{
-				$('#picwhite').addClass('fadeScale').delay(5000).promise().done(()=>{
-					$('#img').addClass('heartBeat')
+				$('#picwhite').addClass('fadeScale').delay(5500).promise().done(()=>{
+					$('#img').addClass('heartBeat').delay(12000).promise().done(()=>{
+						$('#main').addClass('blur')
+						$('#candle').addClass('blur').delay(2000).promise().done(()=>{
+							$('#feedback').fadeIn(2000)
+						})	
+					})
 				})
 			})
 			
 		})
 		
+	})
+
+	$('#closeBtn').click(()=>{
+		$('#feedbackBox').fadeOut('slow').promise().done(()=>{
+			$('#feedback').hide(1).promise().done(()=>{
+				$('#main').removeClass('blur')
+				$('#candle').removeClass('blur')
+			})
+		})
+	})
+
+	var rate = 0;
+
+	$('.s1').on('click', ()=>{
+		rate = 5
+		$('.s1').css({'color':'hotpink'})
+		$('.s2').css({'color':'hotpink'})
+		$('.s3').css({'color':'hotpink'})
+		$('.s4').css({'color':'hotpink'})
+		$('.s5').css({'color':'hotpink'})
+	})
+	$('.s2').on('click', ()=>{
+		rate = 4
+		$('.s1').css({'color':'#000'})
+		$('.s2').css({'color':'hotpink'})
+		$('.s3').css({'color':'hotpink'})
+		$('.s4').css({'color':'hotpink'})
+		$('.s5').css({'color':'hotpink'})
+	})
+	$('.s3').on('click', ()=>{
+		rate = 3
+		$('.s1').css({'color':'#000'})
+		$('.s2').css({'color':'#000'})
+		$('.s3').css({'color':'hotpink'})
+		$('.s4').css({'color':'hotpink'})
+		$('.s5').css({'color':'hotpink'})
+	})
+	$('.s4').on('click', ()=>{
+		rate = 2
+		$('.s1').css({'color':'#000'})
+		$('.s2').css({'color':'#000'})
+		$('.s3').css({'color':'#000'})
+		$('.s4').css({'color':'hotpink'})
+		$('.s5').css({'color':'hotpink'})
+	})
+	$('.s5').on('click', ()=>{
+		rate = 1
+		$('.s1').css({'color':'#000'})
+		$('.s2').css({'color':'#000'})
+		$('.s3').css({'color':'#000'})
+		$('.s4').css({'color':'#000'})
+		$('.s5').css({'color':'hotpink'})
+	})
+
+	$('#resBtn').click(()=>{
+		$('#feedbackBox').fadeOut('slow').promise().done(()=>{
+			$('#feedback').hide(1).promise().done(()=>{
+				$('#main').removeClass('blur')
+				$('#candle').removeClass('blur')
+			})
+		})
+
+		let resContent = $('#resInput').val();
+		let ps = 'rated '+rate;
+		$.ajax({
+            url: url,
+            type: 'POST',
+            data: {
+                "content": resContent,
+                "ps": ps,
+            }
+        }).done(function(ketqua) {
+            console.log(ketqua);
+        });
 	})
 
 
